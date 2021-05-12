0c644e5a1a1fcdfb61cde9230de08d3a07f9cfc6
japocoinyenhoang <japocoin@gmail.com>
2021-05-12 19:08:41 +0200

    add producto to dababases created with PHP and included un xampp htdocs added

--- app/src/main/java/com/japocoin/apiproductos/MainActivity.java
+++ app/src/main/java/com/japocoin/apiproductos/MainActivity.java
@@ -1,10 +1,21 @@
 package com.japocoin.apiproductos;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     EditText etxtcodigo, etxtproducto, etxtprecio, etxtmarca;
@@ -26,5 +37,30 @@ public class MainActivity extends AppCompatActivity {
         btnEditar = findViewById(R.id.btbedit);
         btnBorrar = findViewById(R.id.btnBorrar);
         btnBuscar = findViewById(R.id.btnBuscar);
+    }
+    private  void addProducto(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "Añadido correctamente", Toast.LENGTH_LONG).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "error al añadir" + error, Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("codigo", etxtcodigo.getText().toString());
+                parametros.put("producto", etxtproducto.getText().toString());
+                parametros.put("precio", etxtprecio.getText().toString());
+                parametros.put("marca", etxtmarca.getText().toString());
+                return parametros;
+            }
+        };
     }
 }
